bc1590c4ee2715216e52caa7d2a7c49f285ff1c5
Aihara21 <budiharta_21@live.com>
2016-07-10 00:40:36 +0800

    menghilangkan js yang tidak di perlukan

--- pemilik/view/asset.php
+++ pemilik/view/asset.php
@@ -14,12 +14,6 @@
         
         <script type="text/javascript" src="../../js/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="../../js/materialize.js"></script>
-        
-        
-        <script type="text/javascript" src="../../js/jquery.smoothState.js"></script>
-        <script type="text/javascript" src="../../js/main.js"></script>
-        <link type="text/css" rel="stylesheet" href="../../css/keyframes.css"  media="screen,projection"/>
-        <link type="text/css" rel="stylesheet" href="../../css/pageTransitions.css"  media="screen,projection"/>
     </head>
     <body>
         <?php include "../inc/header.php" ?>
